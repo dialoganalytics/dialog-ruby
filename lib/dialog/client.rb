1af83ce9f588907adbdfe23160d1a24a2117fa33
Philippe Dionne <dionne.phil@gmail.com>
2016-09-22 15:20:50 -0400

    Add link method to client

--- lib/dialog/client.rb
+++ lib/dialog/client.rb
@@ -22,5 +22,13 @@ module Dialog
       end
     end
 
+    # Wraps an url into a trackable Dialog url
+    # @param id [String] A conversation distinct Id provided by the platform or the provider
+    # @param url [String]
+    # @return [String]
+    def link(id, url)
+      escaped = URI.escape(url, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      URI.join(api_endpoint, "v1/click/", id, "?url=#{escaped}").to_s
+    end
   end
 end
