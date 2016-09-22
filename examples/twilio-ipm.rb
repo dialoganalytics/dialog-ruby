eb26e9d19dd367722bc3e836464e706a3d205e88
Philippe Dionne <dionne.phil@gmail.com>
2016-09-22 16:13:35 -0400

    Add blank examples

---
+++ examples/twilio-ipm.rb
@@ -0,0 +1,8 @@
+# This is an example integration of dialog-ruby with twilio-ruby (https://github.com/twilio/twilio-ruby)
+#
+# Get started:
+#   1. Create an account on https://app.dialoganalytics.com
+#   2. Find your API token in your personal settings page and your Bot ID in the bot's settings page
+#   3. Clone this repository and run `bundle install`
+#   4. Run this example by typing `ruby examples/twilio-ipm.rb' from the repository root
+#   5. Read more on how to make the most out of the possibilities offered by Dialog here: https://docs.dialoganalytics.com
