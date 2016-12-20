b8764e8784f293ea8aa05e504d028aa2cde4e469
Philippe Dionne <dionne.phil@gmail.com>
2016-12-20 16:46:24 -0500

    Update track method signature

--- lib/dialog-api/api/track.rb
+++ lib/dialog-api/api/track.rb
@@ -6,27 +6,7 @@ module Dialog
       # @param payload [Hash]
       # @return
       def track(payload)
-        body = {
-          message: {
-            distinct_id: payload[:distinct_id],
-            platform: payload[:platform],
-            provider: payload[:provider],
-            mtype: payload[:mtype],
-            sent_at: payload[:sent_at],
-            properties: {
-              text: payload[:text]
-            }
-          },
-          conversation: {
-            distinct_id: payload[:conversation_distinct_id]
-          },
-          creator: {
-            distinct_id: payload[:creator_distinct_id],
-            type: payload[:creator_type]
-          }
-        }
-
-        post("b/#{bot_id}/track", body: body)
+        post("b/#{bot_id}/track", body: payload)
       end
     end
   end
